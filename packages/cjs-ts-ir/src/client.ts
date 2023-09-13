4a61b33fe3c77f26f2c405a3306da1265fe10857
Mathias Klippinge <mathias.klippinge@gmail.com>
2023-09-13 12:22:24 +0200

    Add specs for cjs-ts-ir

---
+++ packages/cjs-ts-ir/src/client.ts
@@ -0,0 +1,46 @@
+import mappersmith = require("mappersmith");
+const { default: forge, Response, configs, version } = mappersmith;
+import FetchGateway = require("mappersmith/gateway/fetch");
+import _HTTPGateway = require("mappersmith/gateway/http");
+import _MockGateway = require("mappersmith/gateway/mock");
+import _XHRGateway = require("mappersmith/gateway/xhr");
+import TimeoutError = require("mappersmith/gateway/timeout-error");
+const { isTimeoutError, createTimeoutError } = TimeoutError;
+
+import _RetryMiddlewareV1 = require("mappersmith/middleware/retry");
+import _RetryMiddlewareV1Alt = require("mappersmith/middleware/retry/v1");
+import RetryMiddleware = require("mappersmith/middleware/retry/v2");
+import BasicAuth = require("mappersmith/middleware/basic-auth");
+import CSRF = require("mappersmith/middleware/csrf");
+import Duration = require("mappersmith/middleware/duration");
+import EncodeJSON = require("mappersmith/middleware/encode-json");
+import ErrorHandler = require("mappersmith/middleware/global-error-handler");
+import Log = require("mappersmith/middleware/log");
+import Timeout = require("mappersmith/middleware/timeout");
+
+configs.gateway = FetchGateway.default;
+
+export const github = forge({
+  clientId: "github",
+  middleware: [
+    RetryMiddleware.default(),
+    BasicAuth.default({
+      username: "username",
+      password: "password",
+    }),
+    CSRF.default(),
+    Duration.default,
+    EncodeJSON.default,
+    ErrorHandler.default,
+    Log.default,
+    Timeout.default(1000),
+  ],
+  host: "https://www.githubstatus.com",
+  resources: {
+    Status: {
+      current: { path: "/api/v2/status.json" },
+      summary: { path: "/api/v2/summary.json" },
+      components: { path: "/api/v2/components.json" },
+    },
+  },
+});
--- packages/cjs-ts-ir/src/index.ts
+++ packages/cjs-ts-ir/src/index.ts
@@ -1,55 +1,16 @@
 import mappersmith = require("mappersmith");
-const { default: forge, Response, configs, version } = mappersmith;
-import FetchGateway = require("mappersmith/gateway/fetch");
-import _HTTPGateway = require("mappersmith/gateway/http");
-import _MockGateway = require("mappersmith/gateway/mock");
-import _XHRGateway = require("mappersmith/gateway/xhr");
 import TimeoutError = require("mappersmith/gateway/timeout-error");
-const { isTimeoutError, createTimeoutError } = TimeoutError;
+import client = require("./client");
+const { github } = client;
 
-import _RetryMiddlewareV1 = require("mappersmith/middleware/retry");
-import _RetryMiddlewareV1Alt = require("mappersmith/middleware/retry/v1");
-import RetryMiddleware = require("mappersmith/middleware/retry/v2");
-import BasicAuth = require("mappersmith/middleware/basic-auth");
-import CSRF = require("mappersmith/middleware/csrf");
-import Duration = require("mappersmith/middleware/duration");
-import EncodeJSON = require("mappersmith/middleware/encode-json");
-import ErrorHandler = require("mappersmith/middleware/global-error-handler");
-import Log = require("mappersmith/middleware/log");
-import Timeout = require("mappersmith/middleware/timeout");
+const { Response, version } = mappersmith;
+const { isTimeoutError, createTimeoutError } = TimeoutError;
 
 if (isTimeoutError(createTimeoutError("classic timeout error"))) {
   console.log("TimeoutError OK");
 } else {
   throw new Error("TimeoutError not OK");
 }
-
-configs.gateway = FetchGateway.default;
-
-const github = forge({
-  clientId: "github",
-  middleware: [
-    RetryMiddleware.default(),
-    BasicAuth.default({
-      username: "username",
-      password: "password",
-    }),
-    CSRF.default(),
-    Duration.default,
-    EncodeJSON.default,
-    ErrorHandler.default,
-    Log.default,
-    Timeout.default(1000),
-  ],
-  host: "https://www.githubstatus.com",
-  resources: {
-    Status: {
-      current: { path: "/api/v2/status.json" },
-      summary: { path: "/api/v2/summary.json" },
-      components: { path: "/api/v2/components.json" },
-    },
-  },
-});
 
 github.Status.current().then((response) => {
   if (response instanceof Response) {
